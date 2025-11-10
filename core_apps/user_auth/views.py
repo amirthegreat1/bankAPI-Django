0b37d297c29bc8d8c9253f2518025937b66babaa
amirthegreat1 <amirhosseinzhmt@gmail.com>
2025-11-11 00:17:29 +0330

    feat: added loggging with loguru

--- core_apps/user_auth/views.py
+++ core_apps/user_auth/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
-# Create your views here.
+# TEST LOGGING
+# from django.shortcuts import render
+# from django.http import HttpResponse
+# from django.views import View
+# from loguru import logger
+# from django.http import JsonResponse
+# # Create your views here.
+# class TestLogging(View):
+#     def get(self, request):
+#         logger.info("this is a debug message")
+#         logger.error("this is an error message")
+#         logger.warning("this is a warning message")
+#         logger.critical("this is a critical message")
+#         logger.debug("this is a debug message")
+#         return JsonResponse({"message": "we are testing logging"})
