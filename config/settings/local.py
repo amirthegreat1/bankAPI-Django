80f7e0271a5492aff4d845466bdea9548bb69d32
amirthegreat1 <amirhosseinzhmt@gmail.com>
2025-11-17 22:04:05 +0330

    updated csrf trusted urls

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,6 +1,6 @@
-from os import getenv , path
+from os import getenv, path
 from dotenv import load_dotenv
-from .base import * # noqa
+from .base import *  # noqa
 from .base import BASE_DIR
 
 local_env_file = path.join(BASE_DIR, ".envs", ".env.local")
@@ -12,24 +12,26 @@ SECRET_KEY = getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = getenv("DEBUG")
 
-SITE_NAME=getenv("SITE_NAME")
+SITE_NAME = getenv("SITE_NAME")
 
 
-ALLOWED_HOSTS = ["localhost",'127.0.0.1',"0.0.0.0"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
 
-ADMIN_URL=getenv("ADMIN_URL")
+ADMIN_URL = getenv("ADMIN_URL")
 
 EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
-EMAIL_HOST=getenv("EMAIL_HOST")
-EMAIL_PORT=getenv("EMAIL_PORT")
-DEFAULT_FROM_EMAIL=getenv("DEFAULT_FROM_EMAIL")
+EMAIL_HOST = getenv("EMAIL_HOST")
+EMAIL_PORT = getenv("EMAIL_PORT")
+DEFAULT_FROM_EMAIL = getenv("DEFAULT_FROM_EMAIL")
 DOMAIN = getenv("DOMAIN")
 
-MAXIMUM_UPLOAD_SIZE=1*1024*1024
+MAXIMUM_UPLOAD_SIZE = 1 * 1024 * 1024
 
 CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:8080",
     "http://localhost",
     "http://127.0.0.1",
+    "http://127.0.0.1:8080",
     "http://0.0.0.0",
 ]
 LOCKOUT_DURATION = timedelta(minutes=1)
